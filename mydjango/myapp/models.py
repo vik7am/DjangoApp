c3c0191ce9a2acd6ba501fe57bf599d262170328
vik7am <vik7am@gmail.com>
2017-07-17 20:41:33 +0530

    Added modeling in django

--- mydjango/myapp/models.py
+++ mydjango/myapp/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=30)
+    age = models.IntegerField(default=0)
+    has_verified_mobile = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
