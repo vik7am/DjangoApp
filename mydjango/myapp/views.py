d7efe2a022205c6516c3ce983dea6ec912eb99e5
vik7am <vik7am@gmail.com>
2017-07-20 17:41:13 +0530

    Added login page

--- mydjango/myapp/views.py
+++ mydjango/myapp/views.py
@@ -2,15 +2,36 @@
 from __future__ import unicode_literals
 from datetime import datetime
 from django.shortcuts import render
-from forms import SignUpForm
+from forms import SignUpForm, LoginForm
+from django.contrib.auth.hashers import make_password
 
 def signup_view(request):
     if request.method == "POST":
         print 'Sign up form submitted'
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            print "form is valid"
+            username = form.cleaned_data['username']
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            print username+ ":"+ name+ ":"+ email+ ":"+ password+ ":"+ make_password(password)
+            return render(request, 'success.html')
+        else:
+            print "invalid form"
     elif request.method == 'GET':
         form = SignUpForm()
-    today = datetime.now()
-    return render(request, 'index.html', {'form' : form,"today":today})
+        today = datetime.now()
+        return render(request, 'index.html', {'form' : form, "today":today})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+    elif request.method == "GET":
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
 
 '''
 # Create your views here.
--- mydjango/mydjango/urls.py
+++ mydjango/mydjango/urls.py
@@ -15,5 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from myapp.views import signup_view
-urlpatterns = [url('', signup_view)]
+from myapp.views import signup_view, login_view
+
+urlpatterns = [
+    url('login/', login_view),
+    url('', signup_view)
+]
